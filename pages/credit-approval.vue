5bd9d912c0feff3786976bb2c05e850b581d53e8
Frendy <frendy@hawaii.edu>
2020-09-21 13:55:50 -1000

    Credit-Approval V1
    Finished Credit-Approval page V1

--- pages/credit-approval.vue
+++ pages/credit-approval.vue
@@ -1,11 +1,15 @@
-<template>
-  <div>Credit Approval</div>
-</template>
+<style src="../components/credit-approval/credit-approval.css"></style>
+<template src="../components/credit-approval/credit-approval.html"></template>
 
 <script>
+import courses from '../assets/json/courses.json'
 export default {
   name: 'CreditApproval',
+  data(){
+    return{
+      myCourses: courses
+    }
+  },
 }
 </script>
 
-<style></style>
