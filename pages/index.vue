d059c66499217f74adf1b70c25291ab33d158b5f
Willy Chang <willychang@rocketmail.com>
2020-09-22 14:19:22 -1000

    feat: landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>We are here</div>
+  <div>
+    <div class="container">
+      <svg class="block mx-auto mt-20" width="1173" height="647">
+        <circle cx="912" cy="386" r="261" style="fill: #3879c2" />
+        <circle cx="525" cy="272" r="272" style="fill: #38b2c2" />
+        <circle cx="198.5" cy="269.5" r="198.5" style="fill: #38c291" />
+      </svg>
+    </div>
+    <div class="container">
+      <div class="my-auto mx-auto">
+        <div class="border-r-2 border-white">First container</div>
+        <div>Second container</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+.container {
+  margin-right: 0px;
+  width: 1280px;
+  height: 960px;
+}
+</style>
--- store/index.js
+++ store/index.js
@@ -1,3 +1,3 @@
 export const state = () => ({
-  permission: null,
+  permission: null, // user, admin
 })
