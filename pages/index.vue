48b32c17b450294846e4ee121742ba4e8420aa1d
Keenan Lee <49707796+knolee@users.noreply.github.com>
2020-10-15 22:04:10 -1000

    Revert "landing page v1"
    This reverts commit ca866ab4bf00e8e58b377262122887a3d32b0e46.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div />
+  <div>
+    <div class="container">
+      <svg class="block mx-auto mt-20" width="1173" height="647">
+        <circle cx="912" cy="386" r="261" style="fill: #3879c2" />
+        <circle cx="525" cy="272" r="272" style="fill: #38b2c2" />
+        <circle cx="198.5" cy="269.5" r="198.5" style="fill: #38c291" />
+      </svg>
+    </div>
+    <div class="container">
+      <div class="my-auto mx-auto">
+        <div class="border-r-2 border-white">First container</div>
+        <div>Second container</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,10 +25,5 @@ export default {}
   margin-right: 0px;
   width: 1280px;
   height: 960px;
-}
-
-body {
-  background-image: url('../assets/images/landing.png');
-  background-size: 100% 100%;
 }
 </style>
